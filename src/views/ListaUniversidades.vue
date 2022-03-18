d000508b5e9db30595bbd825e8488bff21a74fca
Jainy <jainyestefany@gmail.com>
2022-03-18 09:37:43 -0300

    Esqueleto

---
+++ src/views/ListaUniversidades.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+        <h2>Lista de Universidades</h2>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'ListaUniversidades'
+}
+</script>
+
+<style scoped>
+
+</style>
