729909ee312f4639b4a31b31f01e768773acfe55
Adriano <adriano.junior@ccc.ufcg.edu.br>
2022-03-18 11:10:14 -0300

    Adicionando Toogle Navigation

--- src/components/shared/AppTopBar.vue
+++ src/components/shared/AppTopBar.vue
@@ -1,45 +1,61 @@
 <template>
   <div class="app-top-bar">
     <v-app-bar color="deep-purple accent-4" dark>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
       <v-toolbar-title>Universidades do Reino Unido</v-toolbar-title>
 
-      <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
 
-      <router-link to="/"></router-link>
-      <router-link to="/listauniversidades"></router-link>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
+    <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+    </v-btn>
 
-      <v-menu left bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon v-bind="attrs" v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
+    <v-menu left bottom> </v-menu>
 
-        <v-list>
-          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
     </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense >
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+
+        <router-link to="/">
+            <v-list-item>
+                <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Início</v-list-item-title>
+            </v-list-item>
+        </router-link>
+
+        <router-link to="/listauniversidades">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Lista das Universidades</v-list-item-title>
+          </v-list-item>
+        </router-link>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    
   </div>
 </template>
 
 <script>
 export default {
     name: 'AppTopBar',
+    data: () => ({
+        drawer: false,
+        group: null,
+    }),
 };
 </script>
 
 <style scoped>
+    .app-top-bar a{
+        text-decoration: none;
+    }
 </style>
