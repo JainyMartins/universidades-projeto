b34620c5a0101cd3d3de3e54f5984280bd1cd913
AlineLourenco <alinelaurentius@gmail.com>
2022-03-18 10:27:08 -0300

    atualizando a tela inicio

--- src/views/MeuInicio.vue
+++ src/views/MeuInicio.vue
@@ -1,6 +1,13 @@
 <template>
     <v-container>
-        <h2>Início</h2>
+        <h2 text-h5 text-center mb-3 mt-5>
+            <v-icon>mdi-school</v-icon>
+            Universidades do Reino Unido
+        </h2>
+        <v-img></v-img>
+        <p>Pensando em estudar no Reino Unido e não sabe qual Universidade escolher? Basta acessar a próxima página para ter acesso a uma lista das melhores Universidades para se estudar. 
+        </p>
+        <p>Nessa lista você pode acessar os sites das principais universidades de maneira simples e rápida!</p>
     </v-container>
 </template>
 
