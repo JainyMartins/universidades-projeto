d37335c3da2fd96c823e6fddbe02cf0d4182e303
Lucas Costa <lucascosta9199@gmail.com>
2022-03-18 10:20:22 -0300

    adicionando imagem-inicio

--- src/views/MeuInicio.vue
+++ src/views/MeuInicio.vue
@@ -1,15 +1,22 @@
 <template>
-    <v-container>
-        <h2>Início</h2>
-    </v-container>
+  <v-container>
+    <h2 >Início</h2>
+    <v-img
+      class="inicio-imagem "
+      src="https://st2.depositphotos.com/6193152/10087/i/950/depositphotos_100876046-stock-photo-oxford-university-oxford-united-kingdom.jpg"
+      alt="">
+    </v-img>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name: 'MeuInicio'
-}
+  name: "MeuInicio",
+};
 </script>
 
 <style scoped>
-
+.inicio-imagem {
+  border-radius: 5px;
+}
 </style>
