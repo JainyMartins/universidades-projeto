0727e2615f0bed1aad511d8d4d716b8b6d7bea50
Bruna Finholdt <brunaperez1991@gmail.com>
2022-03-18 10:40:07 -0300

    table

--- src/views/ListaUniversidades.vue
+++ src/views/ListaUniversidades.vue
@@ -1,7 +1,32 @@
 <template>
+
     <v-container>
         <h2>Lista de Universidades</h2>
+
+        <v-simple-table>
+            <template v-slot:default>
+                <thead>
+                    <tr>
+                        <th>Universidade</th>
+                        <th>Domíno</th>
+                        <th>Web Page</th>
+                    </tr>
+                </thead>
+            <tbody>
+                <tr v-for="(universidade, index) of ListaUniversidades" :key="universidade.country">
+                    <td>{{ index + 1 }}</td>
+                    <td>
+                        <span>{{ universidade.name }}</span>
+                    </td>
+                    <td>{{ universidade.domains }}</td>
+                    <td>{{ universidade.web_pages }}
+                </tr>
+            </tbody>
+            </template>
+        </v-simple-table>
+
     </v-container>
+
 </template>
 
 <script>
